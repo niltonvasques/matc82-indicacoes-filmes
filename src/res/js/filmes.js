a3f26ac87331553567ef376d60e4fab994b224a9
Nilton Vasques <nilton.vasques@gmail.com>
2014-11-04 07:41:31 -0300

    Realizado o parser dos dados do html database #4. Carregado o html database através de requisição ajax, utilizando jquery #4. Adicionado bootstrap através do bower.

--- src/res/js/filmes.js
+++ src/res/js/filmes.js
@@ -1,30 +1,63 @@
 var movies_data = "res/data/movies.html";
-var isLocalHost = true;
+var database = null;
+
 function listaFilmes(){
-	var data;
+	var anoField 		= "Ano: " + $('#ano').val();
+	var categoriaField 	= $( '#categoria' ).val();
+	var atoresField 	= $( '#atores' ).val();
+	var informacoesField 	= $( '#informacoes' ).val();
+
+	if(database == null){
+		progress.showPleaseWait();
+		setTimeout(function(){load_database();}, 1000);
+	}else{
+	        document.getElementById("resultados-header").innerHTML = "<th>NOME</th><th>DESCRIÇÃO</th>";
+		$( '#resultados-body' ).empty();
+		var divs = $('div', database);
+		divs.each( function( index, element ){
+				var elementAno = $( 'p', element ).first().text();
+				var nome = $( 'h2', element ).text();
+				var img = $( 'img', element ).first();
+
+				var ulCategorias = $( '.categories', element );
+				var categorias = [];
+				$( 'li', ulCategorias).each( function( cIndex, cElement ){
+					categorias.push( cElement.textContent );
+				});
+	
+				var ulAtores = $( '.actors', element );
+				var atores = [];
+				$( 'li', ulAtores).each( function( aIndex, aElement ){
+					atores.push( aElement.textContent );
+				});
+
+				if( elementAno.contains( anoField ) && 
+					$.inArray(categoriaField, categorias) != -1){
+					console.log(elementAno + " == "+ anoField );
+					$( '#resultados-body' ).append( element.outerHTML );
+				}
+			}
+		);
+	}
+}
+
+function load_database(){
 	var request = $.ajax({
 		url: movies_data,
 		type: "GET",
-		dataType: "html"
+		dataType: "html",
 	});
 	request.done(function( msg ) {
 		console.log("finished" );
-		data = msg;
-//		var movie = $("#1", $(msg));
-//		var movies = document.createElement( "div" );
-		var movies = $('<div></div>');
-		movies.html(msg);
-		var divs = $('div[id="3"]', movies);
-//		movies.innerHTML = msg;
-//		var m = movies.getElementsByTagName("movie");
-		document.getElementById("resultados-body").innerHTML = divs.html();
-
+		database = $('<div></div>');
+		database.html(msg);
+		progress.hidePleaseWait();
 	});
 	request.fail(function( jqXHR, textStatus ) {
 		alert( "Request failed: " + textStatus );
+		progress.hidePleaseWait();
 	});
 
-	document.getElementById("resultados-header").innerHTML = "<th>NOME</th><th>DESCRIÇÃO</th>";
 }
 
 
